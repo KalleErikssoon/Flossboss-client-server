ea681869cdfb7ca2f9fd12d148baf45ee9b90be5
haja <haja@student.chalmers.se>
2023-11-10 13:37:37 +0100

    Update .gitignore to exclude server/config.js

--- server/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  database:
-    "mongodb+srv://flossbossgu:Kuggen2023@flossbossgu.d8jqkjm.mongodb.net/?retryWrites=true&w=majority",
-  hivemq: {
-    URL: "ssl://8f816fbc939b4099b39751f74878bc94.s2.eu.hivemq.cloud:8883/mqtt",
-    Ussername: "flossbossgu",
-    Password: "Kuggen2023",
-  },
-};
