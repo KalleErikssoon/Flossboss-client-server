7db38735962850803f046ef5e697eccfa7393b44
rizwanra <rizwanra@student.chalmers.se>
2023-11-13 00:51:07 +0100

    Create login form with very basic functionality to enter & valid user existance (Frontend)

---
+++ client/src/components/Login.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Login = () => {
+    const [loginData, setLoginData] = useState({
+        name: '',
+        password: ''
+      });
+
+      const handleChange = (e) => {
+        setLoginData({ ...loginData, [e.target.name]: e.target.value });
+      };
+
+      const handleLogin = async (e) => {
+        e.preventDefault();
+
+        try {
+          const response = await axios.post('http://localhost:3000/users/login', loginData);
+          console.log('Login Successful:', response.data);
+    
+          setLoginData({
+            name: '',
+            password: ''
+          });
+    
+        } catch (error) {
+          console.error('Error Logging in user:', error.message);
+        }
+    
+      };    
+
+    return (
+            <div className="container mt-5">
+            <h1 className="mb-4">Login user</h1>
+            <form onSubmit={handleLogin}>
+              <div className="mb-3">
+                <label htmlFor="name" className="form-label">
+                  Name:
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="name"
+                  name="name"
+                  value={loginData.name}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="password" className="form-label">
+                  Password:
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="password"
+                  name="password"
+                  value={loginData.password}
+                  onChange={handleChange}
+                />
+              </div>
+              <button type="submit" className="btn btn-primary">
+                Login
+              </button>
+            </form>
+          </div>
+    );
+};
+
+export default Login;
