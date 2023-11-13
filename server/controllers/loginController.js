a9f9e4b4392463ceb8c6d29ff8b8cbd918572b82
haja <haja@student.chalmers.se>
2023-11-13 15:11:18 +0100

    Modify postman collection test
    Add three tests: 1-Login 2-Log in User does not exist 3-Login user with wrong password

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -4,18 +4,18 @@ const jwt = require("jsonwebtoken");
 class LoginController {
   async login(req, res) {
     try {
-      const username = req.body.username;
+      const email = req.body.email;
       const password = req.body.password;
 
-      const user = await UserModel.findOne({ username });
+      const user = await UserModel.findOne({ email });
 
       if (!user) {
-        return res.status(401).json({ message: "User does not exist" });
+        return res.status(404).json({ message: "User does not exist" });
       } else if (user.password !== password) {
         return res.status(401).json({ message: "Incorrect password" });
       }
 
-      token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, {
+      const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, {
         expiresIn: "1h",
       });
 
