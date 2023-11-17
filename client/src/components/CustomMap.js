29e7eacb71b96a5e1e73968fd375c9df78b6a1b7
haja <haja@student.chalmers.se>
2023-11-17 17:47:48 +0100

    Add the clinics marker in the map

--- client/src/components/CustomMap.js
+++ client/src/components/CustomMap.js
@@ -1,16 +1,35 @@
 import React from "react";
-import { Map, GoogleApiWrapper } from "google-maps-react";
+import axios from "axios";
+
+import { Map, Marker, GoogleApiWrapper } from "google-maps-react";
 const MapContainer = (props) => {
+  const [clinics, setClinics] = React.useState([]);
+
+  React.useEffect(() => {
+    async function getAllClinics() {
+      const response = await axios.get("http://localhost:3000/clinics");
+      setClinics(response.data);
+    }
+    getAllClinics();
+  }, []);
   return (
     <Map
       google={props.google}
       style={{ width: "60%", height: "60%" }}
-      zoom={11}
+      zoom={10}
       initialCenter={{
         lat: 57.7089,
         lng: 11.9746,
       }}
-    />
+    >
+      {clinics.map((clinic) => (
+        <Marker
+          key={clinic._id} // Make sure each clinic has a unique id
+          position={{ lat: clinic.latitude, lng: clinic.longitude }}
+          title={clinic.name}
+        />
+      ))}
+    </Map>
   );
 };
 export default GoogleApiWrapper({
