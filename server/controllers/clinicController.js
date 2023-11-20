8847db44b540d4a8ef88a917d0272be2b973da55
haja <haja@student.chalmers.se>
2023-11-20 16:06:17 +0100

    Add the clinic implentation for the backend
    This commit includes the clinic router, model, and controller

---
+++ server/controllers/clinicController.js
@@ -0,0 +1,17 @@
+const ClinicModel = require("../models/clinic");
+
+class ClinicController {
+  async getAllClinics(req, res) {
+    try {
+      const clinic = await ClinicModel.find({});
+      if (!clinic) {
+        res.status(404).send("No clinics exist");
+      }
+      res.status(200).json(clinic);
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  }
+}
+
+module.exports = ClinicController;
