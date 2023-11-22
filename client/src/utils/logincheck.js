5590de696eaba0ede5b31b22b72d19a37cceffed
Malte Bengtsson <bmalte@student.chalmers.se>
2023-11-22 15:32:34 +0100

    Add comments to utils functions

--- client/src/utils/logincheck.js
+++ client/src/utils/logincheck.js
@@ -1,4 +1,6 @@
 // src/utils/auth.js
+
+//Utility function that checks if a user is logged in or not by looking at the token
 export const loginCheck = () => {
     const token = localStorage.getItem('token');
     return token != null;
--- client/src/utils/logout.js
+++ client/src/utils/logout.js
@@ -1,3 +1,6 @@
+
+//Utility function that handles logging out
+//Clears the localstorage of the session and token
 export const logout = () => {
     localStorage.removeItem('userIdSession');
     localStorage.removeItem('token'); // Clear other stored tokens if needed
