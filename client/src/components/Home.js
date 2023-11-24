cda5273fa8dbbb6ad202974698817d57f605e7c6
rizwanra <rizwanra@student.chalmers.se>
2023-11-24 01:06:31 +0100

    Refactor Login/Register forms
    this commit includes implementation for the validation of login/Register and further combines them to the parent LoginContainer, implements Acceptance criteria:3 & 4 for #16 , see also #6, #7

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,32 +1,20 @@
+/* 
 import React, { useContext } from "react";
-import LoginContainer from '../components/LoginContainer'
+ import LoginContainer from '../components/LoginContainer'
 import { AppContext } from "../context/AppProvider";
-import LogoutButton from "./LogoutButton";
+import LogoutButton from "./LogoutButton"; 
+*/
 
 
 const Home = () => {
 
-    const { showUserModal, setShowUserModal } = useContext(AppContext)
+    //const { showUserModal, setShowUserModal } = useContext(AppContext)
+    // const isSigned = localStorage.getItem('userIdSession')
 
 
     return (
         <>
-            <div style={{ flex: 1 }}>
-                <h1>FlossBoss (Home Page)</h1>
-                <p>Logged in as: {localStorage.getItem('userIdSession')} </p>
-                <button onClick={() => setShowUserModal(prev => !prev)} className="btn btn-primary">
-                    Login/Register
-                </button>
-                <LogoutButton></LogoutButton>
-                
 
-            </div>
-            {showUserModal && (
-                <LoginContainer
-                    handleClose={() => setShowUserModal(false)}
-                    visible={showUserModal}
-                />
-            )}
         </>
     );
 };
