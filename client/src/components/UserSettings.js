db9ba9c5535af39554e51d81a9effaed5bc5b533
Malte Bengtsson <bmalte@student.chalmers.se>
2023-11-26 17:48:28 +0100

    Change from javascript to React-bootstrap
    Changed to Bootstrap React for the UserSetting. Also removed an unnecessary comment in www

--- client/src/components/UserSettings.js
+++ client/src/components/UserSettings.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import UserService from './userService';
+import { Form, Button, Alert } from 'react-bootstrap';
+import userService from './userService';
  
 
 const UserUpdateForm = () => {
@@ -7,40 +8,58 @@ const UserUpdateForm = () => {
     const [phoneNumber, setPhoneNumber] = useState('');
     const [password, setPassword] = useState('');
     const [message, setMessage] = useState('');
+    const [colorVariant, setColorVariant] = useState('success');
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await userService.updateUserInfo( {name, phoneNumber, password});
+            const response = await userService.updateUserInfo({name, phoneNumber, password});
             setMessage(response.data.message);
+            setColorVariant('success');
         } catch (error) {
             setMessage(error.response.data.message);
+            setColorVariant('danger');
         }
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input
-                type="text"
-                placeholder="Name"
-                value={name}
-                onChange={e => setPhoneNumber(e.target.value)}
-            />
-            <input
-                type="tel"
-                placeholder="Phone Number"
-                value={phoneNumber}
-                onChange={e => setPhoneNumber(e.target.value)}
-            />
-            <input
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={e => setPassword(e.target.value)}
-            />
-            <button type="submit">Update</button>
-            {message && <p>{message}</p>}
-        </form>
+        <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="formBasicName">
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                    type="text"
+                    placeholder="Enter name"
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+                />
+            </Form.Group>
+            
+            <Form.Group controlId="formBasicPhoneNumber">
+                <Form.Label>Phone Number</Form.Label>
+                <Form.Control 
+                    type="tel" 
+                    placeholder="Enter phone number" 
+                    value={phoneNumber}
+                    onChange={e => setPhoneNumber(e.target.value)}
+                />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control 
+                    type="password" 
+                    placeholder="Password" 
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+                Update
+            </Button>
+
+            {message && <Alert variant={variant}>{message}</Alert>}
+        </Form>
     );
 };
 
