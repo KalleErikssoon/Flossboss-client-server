f56742e701ecdeedaf22e5d0e3de3470a64befd2
haja <haja@student.chalmers.se>
2023-12-07 10:21:24 +0100

    Add e message to the screen when there is not time slots in the time slots page

--- client/src/components/TimeSlot.js
+++ client/src/components/TimeSlot.js
@@ -2,6 +2,13 @@ import React from "react";
 import "../styles/timeslot.css";
 
 export default function TimeSlot({ onBookClick, timeSlots }) {
+  // Check if the timeSlots array is empty
+  if (timeSlots.length === 0) {
+    return (
+      <p className="no-timeslots">Sorry, there are no time slots available.</p>
+    );
+  }
+
   return (
     <ul className="timeslot-list">
       {timeSlots.map((timeslotObj, index) => (
