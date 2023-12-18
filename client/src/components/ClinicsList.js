56c2076d1468af0cad58ad55777c536409b6f7c8
kaeriks <kaeriks@net.chalmers.se>
2023-12-18 13:34:02 +0100

    Show pop-up for register/login if user tries to book when not logged in
    Show the pop-up for logging in/registering if user tries to book for a clinic and they're not logged in

--- client/src/components/ClinicsList.js
+++ client/src/components/ClinicsList.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, {useContext} from "react";
 import { useNavigate } from "react-router-dom";
+import { AppContext } from '../context/AppProvider';
 import "../styles/clinicsList.css";
+import LoginContainer from './LoginContainer'
+
 function ClinicsList({ clinics, selectedRegion }) {
   const navigate = useNavigate();
+  const { isLoggedIn, showUserModal, setShowUserModal } = useContext(AppContext);
 
   const onBookClick = (name, id) => {
+    if(isLoggedIn) {
     navigate("/booking", { state: { clinicId: id, clinicName: name } });
+    } else {
+      setShowUserModal(true);
+    }
   };
 
   return (
+    <>
     <ul className="clinics-list">
       {clinics.map((clinic, index) => (
         <li key={index} className="clinic-item">
@@ -41,6 +50,12 @@ function ClinicsList({ clinics, selectedRegion }) {
         </li>
       ))}
     </ul>
+    {showUserModal && 
+    <LoginContainer
+    handleClose={() => setShowUserModal(false)}
+    visible={showUserModal}
+    />}
+    </>
   );
 }
 export default ClinicsList;
