79d5f531cef72c1cb46d37fc63bc00f34940dc9e
haja <haja@student.chalmers.se>
2024-01-04 21:34:22 +0100

    Change the background in the booking and myaccount page

--- client/src/pages/MyAccountPage.js
+++ client/src/pages/MyAccountPage.js
@@ -1,8 +1,17 @@
 import React, { useState, useEffect } from "react";
 import axiosInstance from "../axiosInterceptor";
-import { Card, Container, Row, Col, Button, Dropdown, Modal } from "react-bootstrap";
+import {
+  Card,
+  Container,
+  Row,
+  Col,
+  Button,
+  Dropdown,
+  Modal,
+} from "react-bootstrap";
 import MyAccountClinicsList from "../components/MyAccountClinicsList";
 import CalendarComponent from "../components/CalendarUserSubscription";
+import backgroundImage from "../assets/SuggestedBackground.png";
 
 // sets all the dates in the calendar from todays date and 1 month ahead to available
 const generateDatesAvailable = () => {
@@ -14,10 +23,13 @@ const generateDatesAvailable = () => {
   // Calculate the last day of the next month
   const lastDayNextMonth = new Date(currentYear, currentMonth + 2, 0);
 
-  for (let date = new Date(currentDate); date <= lastDayNextMonth; date.setDate(date.getDate() + 1)) {
+  for (
+    let date = new Date(currentDate);
+    date <= lastDayNextMonth;
+    date.setDate(date.getDate() + 1)
+  ) {
     const formattedDate = date.toISOString().split("T")[0];
     dates[formattedDate] = { count: 1 }; // Marking the date as available
-
   }
 
   return dates;
@@ -29,18 +41,20 @@ const MyAccountPage = () => {
   const [clinics, setClinics] = useState([]);
   const [selectedClinic, setSelectedClinic] = useState(null);
   const datesAvailable = generateDatesAvailable();
-  const[showModal, setShowModal] = useState(false);
+  const [showModal, setShowModal] = useState(false);
   const [selectedDate, setSelectedDate] = useState(null);
   const [userEmail, setUserEmail] = useState(null);
 
   useEffect(() => {
     const fetchClinics = async () => {
-        try {
-            const response = await axiosInstance.get("http://localhost:3000/clinics");
-            setClinics(response.data);
-        } catch (error) {
-            console.error("Error fetching clinics:", error);
-        }
+      try {
+        const response = await axiosInstance.get(
+          "http://localhost:3000/clinics"
+        );
+        setClinics(response.data);
+      } catch (error) {
+        console.error("Error fetching clinics:", error);
+      }
     };
     fetchClinics();
   }, []);
@@ -49,7 +63,6 @@ const MyAccountPage = () => {
     console.log("Selected clinic: ", clinic.name, "Clinic id: ", clinic._id);
     setSelectedClinic(clinic);
   };
-
 
   useEffect(() => {
     const userId = localStorage.getItem("userIdSession");
@@ -60,12 +73,11 @@ const MyAccountPage = () => {
 
   useEffect(() => {
     // Fetch user email directly from localStorage
-    const email = localStorage.getItem('Email');
+    const email = localStorage.getItem("Email");
     if (email) {
       setUserEmail(email);
     }
   }, []);
-  
 
   const fetchAppointments = async (userId) => {
     try {
@@ -84,16 +96,14 @@ const MyAccountPage = () => {
   const handleDateSelect = (value) => {
     // Adjust the date for the time zone offset before converting to ISO string
     const offset = value.getTimezoneOffset();
-    const adjustedDate = new Date(value.getTime() - (offset * 60 * 1000));
-    setSelectedDate(adjustedDate.toISOString().split('T')[0]);
+    const adjustedDate = new Date(value.getTime() - offset * 60 * 1000);
+    setSelectedDate(adjustedDate.toISOString().split("T")[0]);
     setShowModal(true);
   };
-  
 
   //A user subscribes to a date belonging to a specific clinic.
   const handleSubscribe = async () => {
-
-    if(!selectedClinic || !selectedDate) {
+    if (!selectedClinic || !selectedDate) {
       console.error("Clinic or date not selected");
       return;
     }
@@ -102,19 +112,20 @@ const MyAccountPage = () => {
       clinicId: selectedClinic._id,
       date: selectedDate,
       email: userEmail,
-      clinicName: selectedClinic.name
+      clinicName: selectedClinic.name,
     };
 
     try {
-      const response = await axiosInstance.put(`http://localhost:3000/clinics/${selectedClinic._id}`, subscriptionData);
+      const response = await axiosInstance.put(
+        `http://localhost:3000/clinics/${selectedClinic._id}`,
+        subscriptionData
+      );
       console.log(response.data);
     } catch (error) {
       console.error("Error subscribing", error);
     }
     setShowModal(false);
-   
   };
-
 
   const handleCancelAppointment = async (appointmentId) => {
     const userId = localStorage.getItem("userIdSession");
@@ -146,78 +157,98 @@ const MyAccountPage = () => {
     return <div>Loading...</div>;
   }
 
+  const backgroundStyle = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    minHeight: "100vh",
+  };
+
   return (
     <>
-    <Container>
-      <h1>My Appointments</h1>
-      <Row>
-        {appointments.map((appointment) => (
-          <Col key={appointment._id} md={12}>
-            <Card className="mb-3">
-              <Card.Body>
-                <Card.Title>{appointment.clinicName}</Card.Title>
-                <div>Date: {appointment.date.slice(0, 10)}</div>
-                <div>Start: {appointment.timeFrom}</div>
-                <div>End: {appointment.timeTo}</div>
-                <div>Clinic Name: {appointment.clinicName}</div>
-                <Button
-                  variant="danger"
-                  onClick={() =>
-                    handleCancelAppointment(
-                      appointment._id,
-                      appointment._clinicId
-                    )
-                  }
-                >
-                  Cancel Appointment
-                </Button>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Row>
-      <Row>
-        <Col md={12}>
-            <h3>Clinics</h3>
-            <Dropdown>
+      <div style={backgroundStyle}>
+        <Container>
+          <h1>My Appointments</h1>
+          <Row>
+            {appointments.map((appointment) => (
+              <Col key={appointment._id} md={12}>
+                <Card className="mb-3">
+                  <Card.Body>
+                    <Card.Title>{appointment.clinicName}</Card.Title>
+                    <div>Date: {appointment.date.slice(0, 10)}</div>
+                    <div>Start: {appointment.timeFrom}</div>
+                    <div>End: {appointment.timeTo}</div>
+                    <div>Clinic Name: {appointment.clinicName}</div>
+                    <Button
+                      variant="danger"
+                      onClick={() =>
+                        handleCancelAppointment(
+                          appointment._id,
+                          appointment._clinicId
+                        )
+                      }
+                    >
+                      Cancel Appointment
+                    </Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row>
+          <Row>
+            <Col md={12}>
+              <h3>Clinics</h3>
+              <Dropdown>
                 <Dropdown.Toggle variant="primary" id="dropdown-basic">
-                    Select Clinic
+                  Select Clinic
                 </Dropdown.Toggle>
-                <Dropdown.Menu style={{ maxHeight: '300px', overflowY: 'auto'}}>
-                    <MyAccountClinicsList clinics={clinics} onClinicSelect={handleClinicSelect} />
+                <Dropdown.Menu
+                  style={{ maxHeight: "300px", overflowY: "auto" }}
+                >
+                  <MyAccountClinicsList
+                    clinics={clinics}
+                    onClinicSelect={handleClinicSelect}
+                  />
                 </Dropdown.Menu>
-            </Dropdown>
-        </Col>
-        {selectedClinic && (
-          <Col md={12}>
-          <h3> Subscribe to a date for {selectedClinic.name} </h3>
-          <CalendarComponent
-          activeStartDate={new Date()} //start from today
-          onDateSelect={(handleDateSelect)}
-          datesAvailable={datesAvailable}
-          />
-          <CalendarComponent //second calendar
-          activeStartDate={new Date(new Date().getFullYear(), new Date().getMonth()+1, 1)} //first day of that month
-          onDateSelect={(handleDateSelect)}
-          />
-          </Col>
-
-        )}
-      </Row>
-    <Modal show={showModal} onHide={() => setShowModal(false)}>
-        <Modal.Header closeButton>
-          <Modal.Title>Subscribe</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          Do you want to subscribe to {selectedDate && selectedDate}?
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="primary" onClick={handleSubscribe}>
-            Subscribe
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </Container>
+              </Dropdown>
+            </Col>
+            {selectedClinic && (
+              <Col md={12}>
+                <h3> Subscribe to a date for {selectedClinic.name} </h3>
+                <CalendarComponent
+                  activeStartDate={new Date()} //start from today
+                  onDateSelect={handleDateSelect}
+                  datesAvailable={datesAvailable}
+                />
+                <CalendarComponent //second calendar
+                  activeStartDate={
+                    new Date(
+                      new Date().getFullYear(),
+                      new Date().getMonth() + 1,
+                      1
+                    )
+                  } //first day of that month
+                  onDateSelect={handleDateSelect}
+                />
+              </Col>
+            )}
+          </Row>
+          <Modal show={showModal} onHide={() => setShowModal(false)}>
+            <Modal.Header closeButton>
+              <Modal.Title>Subscribe</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              Do you want to subscribe to {selectedDate && selectedDate}?
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="primary" onClick={handleSubscribe}>
+                Subscribe
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </Container>
+      </div>
     </>
   );
 };
